4a40fcfee19d9d9494180829531c1d99cae9b69e
tugcetorunn <tokerrtugcee@gmail.com>
2025-01-10 01:10:04 +0300

    Cors policy was added to the backend to connect to our API application and tested from there.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,3 +14,10 @@ export class AppComponent {
   constructor(private toastr: CustomToastrService) {    
   }
 }
+
+// jquery üzerinden apimize istek göndereceğiz. 
+// bu satır ile javascripti kullanarak browser üzerinden hedef endpointe istek gönderebiliriz.
+
+$.get("https://localhost:7166/api/products", data => { // istek neticesinde dönen datayı console a yazdırıyoruz.
+  console.log(data)
+});
